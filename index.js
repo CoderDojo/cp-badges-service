cbcf523483d43b409b8b3ac5b9fb02eaccc78a6d
david-cahill <davidrichardcahill@gmail.com>
2015-07-09 16:24:44 +0100

    award badges to dojo members

--- index.js
+++ index.js
@@ -4,15 +4,13 @@ var seneca = require('seneca')();
 var options = require('./config/seneca-options');
 var cdBadges = require('./lib/cd-badges');
 
-
 seneca.options(options);
 seneca.log.info(
-    'Seneca options',
-    JSON.stringify(seneca.export('options'), null, 4)
+  'Seneca options',
+  JSON.stringify(seneca.export('options'), null, 4)
 );
 
-
 seneca.use(cdBadges);
-
-
-seneca.listen();
+seneca.listen()
+  .client({type: 'web', host: process.env.TARGETIP || '127.0.0.1', port: 10301, pin: 'role:cd-dojos,cmd:*'})
+  .client({type: 'web', host: process.env.TARGETIP || '127.0.0.1', port: 10303, pin: 'role:cd-profiles,cmd:*'});
