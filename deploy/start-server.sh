946ef0ec6fd036c30f24bb2dd748bc32a71020f1
Damian Beresford <dberesford@gmail.com>
2015-08-05 22:47:42 +0100

    Stop app before starting in start.sh

--- deploy/start-server.sh
+++ deploy/start-server.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+isExistApp=`ps -eaf |grep cp-badges-service |grep -v grep| awk '{ print $2; }'`
+if [[ -n $isExistApp ]]; then
+    service cp-badges-service stop
+fi
+
 service cp-badges-service start
