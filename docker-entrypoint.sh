6e278940b7c7b139b77c6deabafa77f00b13eadb
butlerx <butlerx@notthe.cloud>
2017-08-25 14:15:48 +0100

    check if the package file has changed rather then if a node muodules exists

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,11 @@
 #! /usr/bin/env sh
+set -e
 cd /usr/src/app || exit
-if [ ! -d "node_modules" ]; then
+touch .pkg.sha1
+OLD_SHA=$(cat .pkg.sha1)
+NEW_SHA=$(sha1sum package.json)
+if [ "$OLD_SHA" != "$NEW_SHA" ]; then
+  echo "$NEW_SHA" > .pkg.sha1
   npm install
 fi
-nodemon service.js
+npm run dev
